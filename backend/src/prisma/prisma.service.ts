517869eaff552f306c3e5a47b5b309d0797e5dd5
Miguel Bento <miguelbento2000@gmail.com>
2023-07-17 09:42:27 +0200

    Init

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    // Passing the args to the PrismaClient constructor.
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
+}

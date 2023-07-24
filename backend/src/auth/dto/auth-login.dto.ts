88d31c5b102b7582a2a6c957dd1fc2cd8cb76e6b
Miguel Bento <43253459+Kurler3@users.noreply.github.com>
2023-07-24 16:30:33 +0200

    auth

--- backend/src/auth/dto/auth-login.dto.ts
+++ backend/src/auth/dto/auth-login.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class AuthLoginDto {
   @IsEmail()
@@ -6,5 +6,6 @@ export class AuthLoginDto {
   email: string;
   @IsString()
   @IsNotEmpty()
+  @MinLength(6)
   password: string;
 }
